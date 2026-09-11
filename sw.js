3454bcdc4d5424df20d2133d12e4d62356da17fc
tamiclaveria <tami.claveria@gmail.com>
2026-09-11 18:25:49 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* TaFit · Service Worker v8
    Cachea la app y los scripts para que abra al instante, con o sin señal. */
-const CACHE = 'tafit-v8-56';
+const CACHE = 'tafit-v8-58';
 const ASSETS = [
   './',
   './index.html',
